a43290dd034a47382f33c13bc837a40d97ac20e6
mj <mj@cayalas-MacBook-Pro-3.local>
2022-03-26 18:55:29 -0400

    Started views

--- src/view/AnimatorGuiView.java
+++ src/view/AnimatorGuiView.java
@@ -3,16 +3,26 @@ package view;
 import javax.swing.JFrame;
 import model.AnimatorModel;
 
+/**
+ * Represents the visual state of an animator that has shapes as well as commands with a GUI.
+ */
 public class AnimatorGuiView extends JFrame implements IAnimatorView{
   private AnimatorModel model;
   private int width;
   private int height;
 
-  public AnimatorGuiView(AnimatorModel model, int width, int height) {
+  /**
+   * Constructs the animation's shape and command onto the GUI view.
+   * @param model Represents the model to be displayed.
+   * @param width Represents the width of the GUI frame.
+   * @param height Represents the height of the GUI frame.
+   */
+  public AnimatorGuiView(AnimatorModel model) {
     this.model = model;
-    this.width = width;
-    this.height = height;
+    this.width = model.getWidth();
+    this.height = model.getHeight();
   }
+
   @Override
   public void displayAnimation(int tickRate) {
 
--- src/view/AnimatorTextView.java
+++ src/view/AnimatorTextView.java
@@ -3,18 +3,31 @@ package view;
 import java.io.IOException;
 import model.AnimatorModel;
 
+/**
+ * Represents an AnimatorView that display all commands and shapes in a textual format.
+ */
 public class AnimatorTextView implements IAnimatorView{
   private AnimatorModel model;
   private Appendable ap;
 
+  /**
+   * Constructs a view with just a model since no appendable is needed yet without a controller.
+   * @param model Represents the model which the view will display.
+   */
   public AnimatorTextView(AnimatorModel model) {
     this.model = model;
   }
 
+  /**
+   * Constructs a view with an appendable output for when a controller comes into play.
+   * @param model Represents the model which will be displayed.
+   * @param ap Represents the output for the view.
+   */
   public AnimatorTextView(AnimatorModel model, Appendable ap) {
     this.model = model;
     this.ap = ap;
   }
+
   @Override
   public void displayAnimation(int tickRate) {
     try {
